0b779f3633b7bf0e611c54096297c35fe357452d
Paul Sherwood <paul.sherwood@gmail.com>
2021-09-27 23:36:58 +0100

    Angular project structure with partial commit of make transfer component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoggedInGuard } from './guards/logged-in.guard';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [  {
+  path: '',
+  canActivate: [LoggedInGuard],
+  component: HomeComponent
+},
+{
+  path: 'home',
+  canActivate: [LoggedInGuard],
+  component: HomeComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BbUIModule } from './bb-ui/bb-ui.module';
+import { LoggedInGuard } from './guards/logged-in.guard';
+import { HomeComponent } from './home/home.component';
+import { MakeTransferComponent } from './make-transfer/make-transfer.component';
+import { TransactionsListComponent } from './transactions-list/transactions-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MakeTransferComponent,
+    TransactionsListComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    BbUIModule
   ],
-  providers: [],
+  providers: [
+    LoggedInGuard
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
